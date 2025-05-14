8f8d2258d7c01884f8ceb4fa047c1f49cd3d579a
winterviolet07 <sowjanya1479@gmail.com>
2025-05-14 20:39:32 +0530

    Fix: bind to 0.0.0.0 and use dynamic port for Render

--- todo.py
+++ todo.py
@@ -67,4 +67,7 @@ def delete_todo(todo_id):
     return '', 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
