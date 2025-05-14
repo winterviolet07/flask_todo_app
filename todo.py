ef067c7506259da4832c79183633c7225a43a9a7
winterviolet07 <sowjanya1479@gmail.com>
2025-05-14 20:43:56 +0530

    Fix: Bind Flask to 0.0.0.0 with dynamic port for Render

--- todo.py
+++ todo.py
@@ -66,8 +66,8 @@ def delete_todo(todo_id):
     todos = [todo for todo in todos if todo['id'] != todo_id]
     return '', 204
 
-if __name__ == '__main__':
-  import os
+import os
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from Render
+    app.run(host="0.0.0.0", port=port, debug=True)
